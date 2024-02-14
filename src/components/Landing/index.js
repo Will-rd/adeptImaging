5430b07e2060ef88e82686b94948957a94555eb4
Will <wdputnam42@gmail.com>
2024-02-14 17:44:46 -0500

    added some front end components to be filled and stylized later

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -10,7 +10,7 @@ export default function LandingPage() {
                 <h1 className="text-7xl ml-8 text-white row-span-2">
                     Adept Imaging
                     <h2 className="text-white text-2xl mt-2">
-                        Custom screen printing and embroidery
+                       High quality screen printing and embroidery specialists
                     </h2>
                 </h1>
                 <nav className="space-x-4 text-2xl mt-5 mr-60 row-span-2 underline text-white">
@@ -20,10 +20,24 @@ export default function LandingPage() {
                     <a href="/">Contact</a>
                 </nav>
             </section>
-            <aside className="bg-white h-96">
-                
+            <aside className="bg-white w-auto h-auto grid grid-cols-2 gap-4 place-content-center">
+                <div className="w-auto h-auto bg-slate-600 text-white">
+                    and some words for the aside in a div 
+                </div>
+                <div className="w-auto h-auto bg-slate-600 text-white">
+                    another div
+                </div>
+                <div className="w-auto h-auto bg-slate-600 text-white">
+                    even more div
+                </div>
+                <div className="w-auto h-auto bg-slate-600 text-white">
+                    yet another
+                </div>
             </aside>
-
+            <footer className="font-bold bg-sky-600 text-white">
+                Adept Imaging LLC | (706) 955-1120 | 2024<br></br>
+                464 Greene Street Augusta, Ga 30901
+            </footer>
         </main>
     )
 }
